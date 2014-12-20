c5e1bc38a69353d704488464da92c00e2ab70071
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2014-12-20 21:36:27 +0100

    Added comments

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 A little test app with a REST API, using hexastore, deployed on Heroku at https://plusme-crubier.herokuapp.com
+
+Server code in src/server/server.js
--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,18 @@
 var Restify = require('restify');
 var Hexastore = require('hexastore');
 
+
+// Cree le serveur Restify
 var server = Restify.createServer();
+
+// Cree et importe la base de donnee
 var db = new Hexastore();
 db.importZip('palmaresdata');
 
+
+// Reponse aux requetes de type "hello/vincent"
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
 function respond(req, res, next) {
   res.send('hello ' + req.params.name);
   return next();
@@ -12,6 +20,9 @@ function respond(req, res, next) {
 
 
 
+// Reponse aux requetes "athlete/Lecrubier%20Vincent/2009"
+server.get('/athlete/:name/:year', findAthleteYear);
+server.head('/athlete/:name/:year', findAthleteYear);
 function findAthleteYear(req,res,next) {
 var result=db.search([
   [['team'],'has participation',['participation']],
@@ -28,13 +39,7 @@ var result=db.search([
 
 
 
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
-
-server.get('/athlete/:name/:year', findAthleteYear);
-server.head('/athlete/:name/:year', findAthleteYear);
-
-
+// Demarrage du serveur
 server.listen(process.env.PORT || 5000, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
