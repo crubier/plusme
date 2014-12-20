66ab5c8e0b3334c924c97037a10cdd17a4b16877
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2014-12-20 21:22:20 +0100

    Added palmares data

--- src/server/server.js
+++ src/server/server.js
@@ -1,17 +1,40 @@
-var restify = require('restify');
+var Restify = require('restify');
+var Hexastore = require('hexastore');
 
-var server = restify.createServer();
+var server = Restify.createServer();
+var db = new Hexastore();
+db.importZip("palmaresdata");
 
 function respond(req, res, next) {
   res.send('hello ' + req.params.name);
-  next();
+  return next();
 }
 
-var server = restify.createServer();
+
+
+function findAthleteYear(req,res,next) {
+var result=db.search([
+  [["team"],"has participation",["participation"]],
+  [["participation"],"in race",["race"]],
+  [["race"],"in competition",["competition"]],
+  [["competition"],"date",req.params.year],
+  [["competition"],"place",["place"]],
+  [["participation"],"with ranking",["ranking"]],
+  [req.params.name,"in team",["team"]]
+  ]);
+  res.send(JSON.stringify(result));
+}
+
+
+
+
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
+server.get('/athlete/:name/:year', findAthleteYear);
+server.head('/athlete/:name/:year', findAthleteYear);
+
+
 server.listen(process.env.PORT || 5000, function() {
-  console.log(process.env.PORT);
   console.log('%s listening at %s', server.name, server.url);
 });
