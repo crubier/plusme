ee3f585d5229b8dc27623baef5ff5145d5939b55
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2014-12-21 19:36:52 +0100

    Added react on client side

--- src/client/client.js
+++ src/client/client.js
@@ -1,14 +1,7 @@
-// var React=require('react');
+
+
+
 // var Restify = require('restify');
-//
-// var HelloMessage = React.createClass({displayName: "HelloMessage",
-// render: function() {
-//   return React.createElement("div", null, "Hello ", this.props.name);
-// }
-// });
-//
-// React.render(React.createElement(HelloMessage, {name: "John"}), document.getElementById('main'));
-//
 // // Creates a JSON client
 // var client = restify.createJsonClient({
 //   url: 'https://plusme-crubier.herokuapp.com'
@@ -29,4 +22,19 @@ var restApi = new rest.Rest('https://plusme-crubier.herokuapp.com/', {
 
 restApi.read('/hello/Vincent', function(error, data) {
   console.log(data);
+
+  React.render(React.createElement(HelloMessage, {name: data}), document.getElementById('main'));
+
+
+});
+
+
+
+
+var React=require('react');
+
+var HelloMessage = React.createClass({displayName: "HelloMessage",
+render: function() {
+  return React.createElement("div", null, "Hello, i received this : ", this.props.name);
+}
 });
